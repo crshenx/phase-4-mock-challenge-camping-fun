732397d5d33c76d154bc418084da0e551a48c66a
Chris Hendricks <crshenx@gmail.com>
2022-08-16 16:03:10 -0700

    commit tree 47d9cf9733c59b6fe604288b6ab37d4cdbd430e5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid     
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found      
+  private       
+  def record_invalid(e)         
+    render json: {errors: e.record.errors.full_messages }, status: :unprocessable_entity     
+  end      
+  # def record_not_found         
+  #   render json: {error: "Camper not found"}, status: :not_found     
+  # end
 end
